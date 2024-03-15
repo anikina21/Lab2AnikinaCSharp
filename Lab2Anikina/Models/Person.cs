f574c5bab4fb7462d720808593b2e66ef95c594d
Anastasiia Anikina <anikinaanastasiia21@gmail.com>
2024-03-16 01:32:03 +0200

    Add project files.

---
+++ Lab2Anikina/Models/Person.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Lab2Anikina.Models
+{
+    #region enums
+    enum WesternZodiac
+    {
+        Aquarius = 1,
+        Pieces,
+        Aries,
+        Taurus,
+        Gemini,
+        Cancer,
+        Leo,
+        Virgo,
+        Libra,
+        Scorpio,
+        Sagittarius,
+        Capricorn
+    }
+
+    enum ChineseZodiac
+    {
+        Monkey = 0,
+        Rooster,
+        Dog,
+        Pig,
+        Rat,
+        Ox,
+        Tiger,
+        Rabbit,
+        Dragon,
+        Snake,
+        Horse,
+        Sheep
+    }
+    #endregion
+    internal class Person
+    {
+        #region Fields
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private DateTime _birthDate;
+        #endregion
+
+        public Person(string firstName, string lastName, string email, DateTime birthDate)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = email;
+            _birthDate = birthDate;
+        }
+
+        public Person(string firstName, string lastName, string email)
+            : this(firstName, lastName, email, DateTime.MinValue)
+        {
+        }
+
+        public Person(string firstName, string lastName, DateTime birthDate)
+            : this(firstName, lastName, "", birthDate)
+        {
+        }
+
+        public Person() : this("", "", "", DateTime.MinValue){ }
+
+        #region Properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set { _birthDate = value; }
+        }
+
+        public bool IsAdult
+        {
+            get { return CheckIfAdult(); }
+        }
+
+        public string SunSign
+        {
+            get { return findWestZodiac(); }
+        }
+        public string ChineseSign
+        {
+            get { return findChinazodiac(); }
+        }
+
+        public bool IsBirthday
+        {
+            get { return CheckIfBirthDay(); }
+        }
+        #endregion
+
+        private bool CheckIfAdult() 
+        {
+            if (PersonAge() >= 18)
+            {
+                return true;
+            }
+            return false; 
+        }
+
+        private bool CheckIfBirthDay()
+        {
+            if (BirthDate.Day == DateTime.Now.Day && BirthDate.Month == DateTime.Now.Month)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private int PersonAge()
+        {
+            int res = DateTime.Now.Year - this._birthDate.Year;
+            if (DateTime.Now.DayOfYear < this._birthDate.DayOfYear) res -= 1;
+            return res;
+
+        }
+
+        #region findingZodiac
+        private string findChinazodiac()
+        {
+            ChineseZodiac res;
+            if (BirthDate.Month == 1)
+            {
+                if (BirthDate.Year % 12 != 0) res = (ChineseZodiac)(BirthDate.Year % 12 - 1);
+                else res = (ChineseZodiac)12;
+            }
+            else res = (ChineseZodiac)(BirthDate.Year % 12);
+
+            return res.ToString();
+        }
+        private string findWestZodiac()
+        {
+            int m = BirthDate.Month;
+            int d = BirthDate.Day;
+            WesternZodiac res = (WesternZodiac)12;
+            switch (m)
+            {
+                case 1:
+                    if (d < 21) res = (WesternZodiac)12;
+                    else res = (WesternZodiac)1;
+                    break;
+                case 2:
+                    if (d < 20) res = (WesternZodiac)1;
+                    else res = (WesternZodiac)2;
+                    break;
+                case 3:
+                    if (d < 21) res = (WesternZodiac)2;
+                    else res = (WesternZodiac)3;
+                    break;
+                case 4:
+                    if (d < 21) res = (WesternZodiac)3;
+                    else res = (WesternZodiac)4;
+                    break;
+                case 5:
+                    if (d < 22) res = (WesternZodiac)4;
+                    else res = (WesternZodiac)5;
+                    break;
+                case 6:
+                    if (d < 22) res = (WesternZodiac)5;
+                    else res = (WesternZodiac)6;
+                    break;
+                case 7:
+                    if (d < 24) res = (WesternZodiac)6;
+                    else res = (WesternZodiac)7;
+                    break;
+                case 8:
+                    if (d < 24) res = (WesternZodiac)7;
+                    else res = (WesternZodiac)8;
+                    break;
+                case 9:
+                    if (d < 24) res = (WesternZodiac)8;
+                    else    res = (WesternZodiac)9;
+                    break;
+                case 10:
+                    if (d < 24) res = (WesternZodiac)9;
+                    else res = (WesternZodiac)10;
+                    break;
+                case 11:
+                    if (d < 23) res = (WesternZodiac)10;
+                    else res = (WesternZodiac)11;
+                    break;
+                case 12:
+                    if (d < 26) res = (WesternZodiac)11;
+                    else res = (WesternZodiac)12;
+                    break;
+            }
+            return res.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ Lab2Anikina/ViewModels/PersonViewModel.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Lab2Anikina.Models;
+using Lab2Anikina.Views;
+using Lab2Anikina.Tools;
+
+namespace Lab2Anikina.ViewModels
+{
+    internal class PersonViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+        private Person _person = new Person();
+        //private Person _person;
+        private RelayCommand<object> _proceedCommand;
+        private string _info1;
+        private string _info2;
+        private bool _isEnabled = true;
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+       
+
+        #region Properties
+
+        public bool IsEnabled
+        {
+            set { _isEnabled = value; OnPropertyChanged("IsEnabled"); }
+            get { return _isEnabled; }
+        }
+        public string FirstName
+        {
+            get { return _person.FirstName; }
+            set { 
+                _person.FirstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+        public string LastName
+        {
+            get { return _person.LastName; }
+            set { _person.LastName = value; OnPropertyChanged("LastName"); }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return _person.BirthDate; }
+            set { _person.BirthDate = value; OnPropertyChanged("BirthDate"); }
+        }
+
+        public string Email
+        {
+            get { return _person.Email; }
+            set { _person.Email = value; OnPropertyChanged("Email"); }
+        }
+
+        public bool IsAdult
+        {
+            get { return _person.IsAdult; }
+        }
+
+        public string SunSign
+        {
+            get { return _person.SunSign; }
+        }
+        public string ChineseSign
+        {
+            get { return _person.ChineseSign; }
+        }
+
+        public bool IsBirthday
+        {
+            get { return _person.IsBirthday; }
+        }
+
+
+        public string Info1
+        {
+            set
+            {
+                _info1 = value;
+                OnPropertyChanged("Info1");
+            }
+            get { return _info1; }
+        }
+
+
+        public string Info2
+        {
+            set
+            {
+                _info2 = value;
+                OnPropertyChanged("Info2");
+            }
+            get { return _info2; }
+        }
+
+        #endregion
+
+        public RelayCommand<object> ProceedCommand
+        {
+            get
+            {
+                return _proceedCommand ??= new RelayCommand<object>(_ => Proceed(), CanExecute);
+            }
+        }
+
+        private async void Proceed()
+        {
+            IsEnabled = false;
+            await Task.Run(() =>
+            {
+                if (BirthDate.CompareTo(DateTime.Now) > 0)
+                {
+                    MessageBox.Show("Error\nHaven't been born yet?");
+
+                }
+                else
+                if (DateTime.Now.Year - BirthDate.Year > 136)
+                {
+                    MessageBox.Show("Error\nToo old to be true");
+
+                }
+                else
+                {
+                    if (IsBirthday)
+                    {
+                        MessageBox.Show("HAPPY BIRTHDAY!!!");
+                    }
+                    Task.Delay(2000).Wait();
+
+                    Info1 = $"First name: {FirstName} \nLast name: {LastName}\n" +
+                        $"Birthday: {BirthDate.Date.ToString("dd/MM/yyyy")} \nEmail: {Email}";
+                    Info2 = $"IsAdult: {IsAdult} \nSunSign: {SunSign}\n" +
+                        $"ChineseSign: {ChineseSign} \nIsBirthday: {IsBirthday}";
+                }
+            });
+            IsEnabled = true;
+        }
+
+
+        private bool CanExecute(object obj)
+        {
+            //return true;
+            return !String.IsNullOrWhiteSpace(_person.FirstName) &&
+                !String.IsNullOrWhiteSpace(_person.LastName) &&
+                (DateTime.MinValue != _person.BirthDate) &&
+                !String.IsNullOrWhiteSpace(_person.Email);
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
